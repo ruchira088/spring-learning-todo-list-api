f3f658cc8c53544863d68f30cee4e0be5e1ce203
Ruchira (Richie) Jayasekara <me@ruchij.com>
2021-08-14 22:29:20 +1000

    Adding the Flayway migration scripts

---
+++ src/main/resources/db/migration/V001__create_todo_list_item_table.sql
@@ -0,0 +1,10 @@
+
+CREATE table todo_list_items(
+    id VARCHAR(40) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    status VARCHAR(24) NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    description TEXT NULL,
+    completed_at TIMESTAMP NULL,
+    PRIMARY KEY (id)
+)
---
+++ src/test/kotlin/com/ruchij/TodoListApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ruchij
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TodoListApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
